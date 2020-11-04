029bf36964481e3446dc268e3b5f15a9e356b6d3
Alexis <alexis.trousset@yahoo.fr>
2020-11-04 16:06:35 +0100

    :sparkles: Add api to know if email address is used

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -10,6 +10,7 @@ const app = express();
 
 const db = require ('./helpers/db/db');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- controllers/UserControllers/isUsedEmail.js
+++ controllers/UserControllers/isUsedEmail.js
@@ -3,8 +3,8 @@
 const UserServices = require('../../services/UserServices');
 
 const isUsedEmail = function (req, res, next) {
-    console.log('UserController - isUsedEmail - start: ', req.body.email);
-    UserServices.isUsedEmailAddress(req.body.email).then((response) => {
+    console.log('UserController - isUsedEmail - start: ', req.query.email);
+    UserServices.isUsedEmailAddress(req.query.email).then((response) => {
         console.log('UserController - isUsedEmail - end');
         res.json({
           status: 'success',
